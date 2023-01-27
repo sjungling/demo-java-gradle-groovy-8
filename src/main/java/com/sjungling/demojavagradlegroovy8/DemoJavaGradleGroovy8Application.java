2822bf01c8e551d928356579dc933df7eed0f62d
Scott Jungling <scott@moderne.io>
2023-01-26 20:13:28 -0800

    chore: init

---
+++ src/main/java/com/sjungling/demojavagradlegroovy8/DemoJavaGradleGroovy8Application.java
@@ -0,0 +1,13 @@
+package com.sjungling.demojavagradlegroovy8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaGradleGroovy8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaGradleGroovy8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjungling/demojavagradlegroovy8/DemoJavaGradleGroovy8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjungling.demojavagradlegroovy8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaGradleGroovy8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
